6e83ec698b1559557efa8bcbc0528615eaf210e5
arindakusumawardani <arindawardani47@gmail.com>
2021-03-09 09:03:32 +0700

    tampil hasil di ui

--- CalculatorWithDI/app/src/main/java/com/pascalarin/calculatorwithdi/presentation/MainActivity.kt
+++ CalculatorWithDI/app/src/main/java/com/pascalarin/calculatorwithdi/presentation/MainActivity.kt
@@ -1,33 +1,51 @@
 package com.pascalarin.calculatorwithdi.presentation
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
+import com.enigmacamp.myhiltcalculator.data.model.CalculatorRequest
 import com.pascalarin.calculatorwithdi.R
 import com.pascalarin.calculatorwithdi.data.repository.CalculatorRepositoryPostImpl
 import com.pascalarin.calculatorwithdi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var calculateRequest : CalculatorRequest? = null
     private lateinit var viewModel: MainActivityViewModel
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initViewModel()
-
         binding.apply {
-            calcButton.setOnClickListener {
-                viewModel.doCalculatorCount().observe(this@MainActivity, {
-                    Log.d("calc", it)
+            calculateRequest?.apply {
+                countBtn.text = "POST"
+
+                number1Tiet.setText(number1)
+                number2Tiet.setText(number2)
+                operatorTiet.setText(operator)
+            }
+            countBtn.setOnClickListener{
+                calculateRequest = CalculatorRequest(
+                    number1 = number1Et.editText?.text.toString(),
+                    number2 = number2Et.editText?.text.toString(),
+                    operator = operatorEt.editText?.text.toString()
+                )
+                viewModel.validation(calculateRequest!!).observe(this@MainActivity, Observer{
+                    binding.hasil.text = it
                 })
             }
         }
+        setContentView(binding.root)
+        initViewModel()
     }
 
     private fun initViewModel() {
--- CalculatorWithDI/app/src/main/java/com/pascalarin/calculatorwithdi/presentation/MainActivityViewModel.kt
+++ CalculatorWithDI/app/src/main/java/com/pascalarin/calculatorwithdi/presentation/MainActivityViewModel.kt
@@ -1,28 +1,46 @@
 package com.pascalarin.calculatorwithdi.presentation
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.enigmacamp.myhiltcalculator.data.model.CalculatorRequest
 import com.pascalarin.calculatorwithdi.data.repository.CalculatorRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withTimeout
 
-class MainActivityViewModel(private val repository : CalculatorRepository) : ViewModel() {
+class MainActivityViewModel(private val repository: CalculatorRepository) : ViewModel() {
 
-    fun doCalculatorCount() = liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
-        withTimeout(2000) {
-            try {
-                emit("Please Wait !")
-                val response = repository.doCount(CalculatorRequest(number1 = "8", number2 = "2", operator = "/"))
-                emit(response.result).toString()
-            }catch (e:Exception) {
-                Log.d("Calc", e.toString())
-                emit("Error")
-            }finally {
-                emit("Done")
+//    private var _mutableResult = MutableLiveData<String>()
+//    val mutableResult: LiveData<String>
+//        get() {
+//            return _mutableResult
+//        }
+
+    fun calculatorCount(calculatorRequest: CalculatorRequest) =
+            liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
+                withTimeout(2000) {
+                    try {
+//                        emit("Please Wait !")
+                        val response = repository.doCount(calculatorRequest)
+                        emit(response.result).toString()
+                    } catch (e: Exception) {
+                        Log.d("Calc", e.toString())
+                        emit("Error")
+                    }
+                }
             }
-        }
-    }
+
+    fun validation(request: CalculatorRequest, ) =
+            when (request.operator) {
+                "+", "-", "*", "/" -> calculatorCount(request)
+                else -> liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
+                    withTimeout(1000) {
+                        try {
+                            emit("Wrong Input")
+                        } catch (e: Exception) {
+                            Log.d("Calc", e.toString())
+                            emit("error")
+                        }
+                    }
+                }
+            }
 }
